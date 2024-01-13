d8c0136a17cffccaa91b9d9500d5e7f0498fd1e2
0vitor <w.wutaes@gmail.com>
2024-01-12 21:39:32 -0300

    remove time date

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
-import { useEffect, useState } from "react";
 import { Home } from "./pages/home";
 
 function App() {
-  const [dataHora, setDataHora] = useState("");
-
-  function atualizarDataEHora() {
-    const dataAtual = new Date();
-    const dataFormatada = dataAtual.toLocaleDateString();
-    const horaFormatada = dataAtual.toLocaleTimeString();
-    const dataHoraFormatada = `${dataFormatada} ${horaFormatada}`;
-    setDataHora(dataHoraFormatada);
-  }
-
-  useEffect(() => {
-    const setIntervalId = setInterval(atualizarDataEHora, 1000);
-
-    return () => clearInterval(setIntervalId);
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Just do it.</h1>
+    <div className="bg-slate-100 w-screen h-screen">
+      <Toaster />
+      <h1 >Just do it.</h1>
 
-      <input type="text" placeholder="Add a task" />
-
-      <div id="data-hora">
-        <span>{dataHora}</span>
-      </div>
-
-      <div id="lista">
-       <Home />
-      </div>
+      <Home />
     </div>
   );
 }

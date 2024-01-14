bd56ee2d5413db8f1c66f8fe1f0a2b6d7262ecf5
0vitor <w.wutaes@gmail.com>
2024-01-14 18:26:21 -0300

    replace drag and drop implementation

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,6 +1,7 @@
-import { DragEvent, useEffect } from "react";
 import { Task } from "../interfaces/Task";
+import React from 'react'
 import { v4 as uuidv4 } from "uuid";
+import { useDrag, useDrop } from "react-dnd";
 
 interface taskMethods {
   tasks: Task[];
@@ -8,131 +9,138 @@ interface taskMethods {
 }
 
 function ListTasks({ tasks, setTasks }: taskMethods) {
-  const dragStart = (event) => {
-    if (event.target.className.includes("card")) {
-      event.target.classList.add("dragging");
-    }
-  };
-
-  const dragEnd = (event) => {
-    if (event.target.className.includes("card")) {
-      event.target.classList.remove("dragging");
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("dragstart", dragStart);
-    document.addEventListener("dragend", dragEnd);
-
-    return () => {
-      document.removeEventListener("dragstart", dragStart);
-      document.removeEventListener("dragend", dragEnd);
-    };
-  }, []);
-
-  const dragEnter = (event: DragEvent<HTMLElement>) => {
-    event.currentTarget.classList.add("drop");
-  };
-
-  const dragLeave = (event: DragEvent<HTMLElement>) => {
-    event.currentTarget.classList.remove("drop");
-  };
-
-  const drag = (event: DragEvent<HTMLElement>, taskId: string) => {
-    event.dataTransfer.setData("text/plain", taskId);
-  };
-
-  const drop = (event: DragEvent<HTMLDivElement>, column: string) => {
-    const taskId = event.dataTransfer.getData("text/plain");
-    event.currentTarget.classList.remove("drop");
-
-    event.preventDefault();
-
-    const updatedState = tasks.map((task) =>
-      task.id === taskId ? { ...task, state: column } : task
-    );
-
-    setTasks(updatedState);
-  };
-
-  const allowDrop = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-  };
-
-  const dragDropMethods = {
-    allowDrop,
-    drop,
-    drag,
-    dragEnd,
-    dragEnter,
-    dragLeave,
-    dragStart,
-  };
 
   const status = ["todo", "in-progress", "done"];
+
   return (
-    <div className="flex justify-center gap-16">
-        {status.map((state) => (
-          <div className="flex flex-col">
-            <Header state={state} />
-            <List
-              key={uuidv4()}
-              dragDropMethods={dragDropMethods}
-              status={state}
-              tasks={tasks}
-            />
-          </div>
-        ))}
+    <div className="flex justify-center gap-16" key={uuidv4()}>
+      {status.map((state) => (
+        <div className="flex flex-col" key={uuidv4()}>
+          <TaskList
+            key={uuidv4()}
+            state={state}
+            tasks={tasks}
+            setTasks={setTasks}
+          />
+        </div>
+      ))}
     </div>
   );
 }
 
-const Header = ({ state }) => {
-  let bg = "bg-slate-500"
+const TaskList = ({
+  state,
+  tasks,
+  setTasks,
+}) => {
 
-  if (state == 'in-progress') {
-    bg = "bg-purple-500"
+  const addItemToSection = (id) => {
+    console.log(id)
+
+    const newTasks = tasks.map(task => {
+      if(task.id === id) {
+        return {...task, state}
+      }
+      return task
+    })
+
+    console.log(tasks)
+    setTasks(newTasks)
+    console.log(tasks)
+
   }
 
-  if(state == 'done') {
-    bg = 'bg-green-500'
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "task",
+    drop: (item: Task) => addItemToSection(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver()
+    })
+  }))
+
+  return (
+  <div
+    ref={drop}
+    className={`h-full ${isOver ? "bg-slate-200": ""}`}
+    data-column={state}
+  >
+    <Header state={state} key={uuidv4()} />
+    {tasks
+      .filter((task: Task) => task.state === state)
+      .map((task: Task) => (
+        <Task
+          task={task}
+          tasks={tasks}
+          setTasks={setTasks}
+          key={task.id}
+        />
+      ))}
+  </div>
+)};
+
+const Header = ({ state }) => {
+  let bg = "bg-slate-500";
+
+  if (state == "in-progress") {
+    bg = "bg-purple-500";
+  }
+
+  if (state == "done") {
+    bg = "bg-green-500";
   }
 
   return (
-    <div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white w-64`}>
+    <div
+      className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white w-64`}
+    >
       <h2>{state}</h2>
     </div>
   );
 };
 
-const List = ({ dragDropMethods, status, tasks }) => (
-  <div
-    className=""
-    data-column={status}
-    onDragEnter={dragDropMethods.dragEnter}
-    onDragLeave={dragDropMethods.dragLeave}
-    onDragOver={dragDropMethods.allowDrop}
-    onDrop={(event) => dragDropMethods.drop(event, status)}
-  >
-    {tasks
-      .filter((task: Task) => task.state === status)
-      .map((task: Task) => (
-        <Task drag={dragDropMethods.drag} task={task}/>
-      ))}
-  </div>
-);
+const Task = ({ task, tasks, setTasks }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "task",
+    item: {id: task.id},
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
 
-const Task = ({drag, task}) => {
-  return (<div
-        className="relative p-4 mt-8 shadow-md rounded-md cursor-grabbing"
-        key={task.id}
-        draggable="true"
-        onDragStart={(event) => drag(event, task.id)}
-        data-id={task.id}
+  const removeTask = (id) => {
+    const fTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(fTasks);
+  };
+
+  return (
+    <div
+      className={`relative p-4 mt-8 shadow-md rounded-md cursor-grabbing ${isDragging ? "opacity-25": "opacity-100"}`}
+      ref={drag}
+    >
+      <h3>{task.name}</h3>
+      <p>{task.description}</p>
+      <button
+        className="absolute right-1 bottom-1 text-slate-400"
+        onClick={() => removeTask(task.id)}
       >
-        <h3>{task.name}</h3>
-        <p>{task.description}</p>
-      </div>
-    )
-}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
 export { ListTasks };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,26 +4,26 @@ import { Task } from '../interfaces/Task';
 import { ListTasks } from '../components/ListTasks';
 
 function Home() {
-  const [query, setQuery] = useState('');
+  // const [query, setQuery] = useState('');
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  const filteredTasks: Task[] = tasks.filter((task) => {
+  /* const filteredTasks: Task[] = tasks.filter((task) => {
     return task.name.includes(query);
   });
 
   function handleChangeSearch(query: string) {
     setQuery(query);
   }
-
+ */
   return (
-    <div className=''>
+    <div className='bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16 pt-32'>
 
-      <input
+      {/* <input
         onChange={(e) => handleChangeSearch(e.target.value)}
         value={query}
         type="text"
         placeholder="escreva sua pesquisa"
-      />
+      /> */}
 
       <CreateTask tasks={tasks} setTasks={setTasks}/>
       <ListTasks tasks={tasks} setTasks={setTasks}/>
